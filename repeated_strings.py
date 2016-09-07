e4b0d6456d25b48f3fe35634bb485cdd442addef
palaxi0 <palaxis08@gmail.com>
2016-09-06 19:19:31 -0500

    Update repeated_strings.py

--- repeated_strings.py
+++ repeated_strings.py
@@ -1,14 +1,14 @@
-"""Get the longest substring that appears more than once """
+""" Program to get the longest repeated substring """
 import sys
 with open(sys.argv[1], 'r') as test_cases:
     for test in test_cases:
         line = test.strip()
         substrings = []
-        for i in range(0, len(line)):
-            for j in range(i+1, len(line)):
-                if line[i:j+1] not in substrings and line.count(line[i:j+1])> 1:
-                    if line[i:j+1].replace(" ", "") != "":
-                        substrings.append(line[i:j+1].replace(" ", ""))
+        for i in range(0,len(line)):
+            for j in range(i+1,len(line)):
+                if line[i:j+1].strip() not in substrings and line.count(line[i:j+1].strip())> 1:
+                    if line[i:j+1].strip() != "":
+                        substrings.append(line[i:j+1].strip())
         if len(substrings) > 0:
             print max(substrings,key=len)
         else:
